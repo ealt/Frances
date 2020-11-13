d8f1ffca10102bde60a1a89cab435f5f77386cbf
Eric <eric@eric.fios-router.home>
2020-11-12 22:34:36 -0600

    [feat] Add windows with encoder
    resolves #6

--- puzzle_encoder.py
+++ puzzle_encoder.py
@@ -17,5 +17,21 @@ class PuzzleEncoder:
     def set_floor_plan(self, floor_plan):
         self._puzzle.crime_scene.floor_plan[:] = floor_plan
 
+    def add_window(self, row=None, column=None, top=None, bottom=None,
+                   left=None, right=None, **kwargs):
+        window = self._puzzle.crime_scene.windows.add()
+        if row is not None:
+            window.vertical_border.row = row
+            if left is not None:
+                window.vertical_border.left = left
+            if right is not None:
+                window.vertical_border.right = right
+        if column is not None:
+            window.horizontal_border.column = column
+            if top is not None:
+                window.horizontal_border.top = top
+            if bottom is not None:
+                window.horizontal_border.bottom = bottom
+
     def get_puzzle(self):
         return self._puzzle
