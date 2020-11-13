703f30eaeeeeee3f06f478146684b06d0b72094e
Eric <eric@eric.fios-router.home>
2020-11-12 22:20:39 -0600

    [feat] Add rooms with encoder
    resolves #4

--- puzzle_encoder.py
+++ puzzle_encoder.py
@@ -4,6 +4,15 @@ from puzzle_pb2 import Puzzle
 class PuzzleEncoder:
     def __init__(self, name=''):
         self._puzzle = Puzzle(name=name)
+    
+    def set_rooms(self, room_names):
+        del self._puzzle.crime_scene.rooms[:]
+        for room_id, room_name in enumerate(room_names):
+            _ = self._puzzle.crime_scene.rooms.add(id=room_id, name=room_name)
+        self._room_ids = {
+            room_name.lower(): room_id
+            for room_id, room_name in enumerate(room_names)
+        }
 
     def get_puzzle(self):
         return self._puzzle
