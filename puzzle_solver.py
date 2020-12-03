4ee0df479f5e11475d862c46eda21fb7a1f2c2a3
Eric <ericallenalt@gmail.com>
2020-12-02 21:04:27 -0600

    [feat] Creat model of windows in solver
    #13

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -4,6 +4,7 @@ from puzzle_pb2 import Puzzle
 class Space:
     def __init__(self, room_id):
         self.room_id = room_id
+        self.beside = set()
 
 
 class Board:
@@ -11,6 +12,7 @@ class Board:
         self._n = n
         self._crime_scene = crime_scene
         self._init_spaces()
+        self._add_windows()
 
     def _init_spaces(self):
         self._spaces = [[
@@ -21,6 +23,31 @@ class Board:
     def _get_room_id(self, row, column):
         index = (self._n * row) + column
         return self._crime_scene.floor_plan[index]
+
+    def _add_windows(self):
+        for window in self._crime_scene.windows:
+            if window.HasField('vertical_border'):
+                self._add_vertical_window(window.vertical_border)
+            if window.HasField('horizontal_border'):
+                self._add_horizontal_window(window.horizontal_border)
+
+    def _add_vertical_window(self, vertical_border):
+        row = vertical_border.row
+        if vertical_border.left is not None:
+            column = vertical_border.left
+            self._spaces[row][column].beside.add('window')
+        if vertical_border.right is not None:
+            column = vertical_border.right
+            self._spaces[row][column].beside.add('window')
+            
+    def _add_horizontal_window(self, horizontal_border):
+        column = horizontal_border.column
+        if horizontal_border.top is not None:
+            row = horizontal_border.top
+            self._spaces[row][column].beside.add('window')
+        if horizontal_border.bottom is not None:
+            row = horizontal_border.bottom
+            self._spaces[row][column].beside.add('window')
 
 
 class PuzzleSolver:
