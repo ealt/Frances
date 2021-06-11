bc9cfd7722ea90e7847a2d3aa3e28d425b533539
Eric Alt <ericallenalt@gmail.com>
2021-06-10 22:07:04 -0500

    [feat] Add width variable to visualizer
    The aspect ratio of single width spaces is very skewed
    
    With a 3:1 ratio of characters the board looks closer to a sqaure
    
    Made width a variable `_w` that can easily adjusted going forward

--- puzzle_visualizer.py
+++ puzzle_visualizer.py
@@ -5,8 +5,9 @@ ROW = lambda n, value: [value if c % 2 == 1 else ' ' for c in range(2 * n + 1)]
 
 class PuzzleVisualizer:
 
-    def __init__(self, puzzle: Puzzle) -> None:
+    def __init__(self, puzzle: Puzzle, w: int = 3) -> None:
         self._puzzle = puzzle
+        self._w = w
         self._n = len(self._puzzle.people)
         self._board = []
         self._add_exterior_walls()
@@ -18,8 +19,8 @@ class PuzzleVisualizer:
 
     def _add_exterior_walls(self) -> None:
         vertical_wall_value = '\u2502'
-        horizontal_wall_value = '\u2500'
-        horizontal_empty_value = ' '
+        horizontal_wall_value = self._w * '\u2500'
+        horizontal_empty_value = self._w * ' '
         self._board.append(ROW(self._n, horizontal_wall_value))
         for r in range(1, 2 * self._n):
             self._board.append(ROW(self._n, horizontal_empty_value))
