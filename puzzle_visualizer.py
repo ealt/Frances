01d37462f800a51fabe6236d893e8ae19122974d
Eric Alt <ericallenalt@gmail.com>
2021-06-10 22:01:46 -0500

    [feat] Add exterior walls to visualizer
    resolves #51

--- puzzle_visualizer.py
+++ puzzle_visualizer.py
@@ -1,16 +1,32 @@
 from puzzle_pb2 import Puzzle
 
+ROW = lambda n, value: [value if c % 2 == 1 else ' ' for c in range(2 * n + 1)]
+
 
 class PuzzleVisualizer:
 
     def __init__(self, puzzle: Puzzle) -> None:
         self._puzzle = puzzle
+        self._n = len(self._puzzle.people)
         self._board = []
+        self._add_exterior_walls()
         self._set_visulization()
 
     @property
     def visualization(self) -> str:
         return self._visualization
+
+    def _add_exterior_walls(self) -> None:
+        vertical_wall_value = '\u2502'
+        horizontal_wall_value = '\u2500'
+        horizontal_empty_value = ' '
+        self._board.append(ROW(self._n, horizontal_wall_value))
+        for r in range(1, 2 * self._n):
+            self._board.append(ROW(self._n, horizontal_empty_value))
+            if r % 2 == 1:
+                self._board[-1][0] = vertical_wall_value
+                self._board[-1][-1] = vertical_wall_value
+        self._board.append(ROW(self._n, horizontal_wall_value))
 
     def _set_visulization(self):
         self._visualization = '\n'.join([''.join(row) for row in self._board])
