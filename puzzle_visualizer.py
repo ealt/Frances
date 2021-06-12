5ffe390046ad5f608e1d3954135c007b35f8ca66
Eric Alt <ericallenalt@gmail.com>
2021-06-11 23:15:37 -0500

    [feat] Add wall intersections to visualization
    resolves #53

--- puzzle_visualizer.py
+++ puzzle_visualizer.py
@@ -1,4 +1,29 @@
+from collections import namedtuple
+
 from puzzle_pb2 import Puzzle
+
+WallIntersection = namedtuple('WallIntersection',
+                              ['up', 'down', 'left', 'right'],
+                              defaults=[False, False, False, False])
+
+WALL_INTERSECTION_VALUES = {
+    WallIntersection(): ' ',
+    WallIntersection(left=True, right=True): '\u2500',
+    WallIntersection(up=True, down=True): '\u2502',
+    WallIntersection(down=True, right=True): '\u250C',
+    WallIntersection(down=True, left=True): '\u2510',
+    WallIntersection(up=True, right=True): '\u2514',
+    WallIntersection(up=True, left=True): '\u2518',
+    WallIntersection(up=True, down=True, right=True): '\u251C',
+    WallIntersection(up=True, down=True, left=True): '\u2524',
+    WallIntersection(down=True, left=True, right=True): '\u252C',
+    WallIntersection(up=True, left=True, right=True): '\u2534',
+    WallIntersection(up=True, down=True, left=True, right=True): '\u253C',
+    WallIntersection(left=True): '\u2574',
+    WallIntersection(up=True): '\u2575',
+    WallIntersection(right=True): '\u2576',
+    WallIntersection(down=True): '\u2577',
+}
 
 ROW = lambda n, value: [value if c % 2 == 1 else ' ' for c in range(2 * n + 1)]
 
@@ -12,6 +37,7 @@ class PuzzleVisualizer:
         self._board = []
         self._add_exterior_walls()
         self._add_interior_walls()
+        self._add_wall_intersections()
         self._set_visulization()
 
     @property
@@ -53,13 +79,49 @@ class PuzzleVisualizer:
                     self._set_horizontal_boundary_value(r, c,
                                                         horizontal_wall_value)
 
+    def _add_wall_intersections(self) -> None:
+        for bottom in range(self._n + 1):
+            for right in range(self._n + 1):
+                wall_intersection = self._get_wall_intersection(bottom, right)
+                value = WALL_INTERSECTION_VALUES[wall_intersection]
+                self._set_boundary_intersection_value(bottom, right, value)
+
+    def _get_wall_intersection(self, bottom: int,
+                               right: int) -> WallIntersection:
+        top = bottom - 1
+        left = right - 1
+        vertical_wall_value = '\u2502'
+        horizontal_wall_value = self._w * '\u2500'
+        up_wall = False if bottom == 0 else self._get_vertical_boundary_value(
+            top, right) == vertical_wall_value
+        down_wall = False if bottom == self._n else self._get_vertical_boundary_value(
+            bottom, right) == vertical_wall_value
+        left_wall = False if right == 0 else self._get_horizontal_boundary_value(
+            bottom, left) == horizontal_wall_value
+        right_wall = False if right == self._n else self._get_horizontal_boundary_value(
+            bottom, right) == horizontal_wall_value
+        return WallIntersection(up=up_wall,
+                                down=down_wall,
+                                left=left_wall,
+                                right=right_wall)
+
+    def _get_vertical_boundary_value(self, row: int, right: int) -> str:
+        return self._board[2 * row + 1][2 * right]
+
     def _set_vertical_boundary_value(self, row: int, right: int,
                                      value: str) -> None:
         self._board[2 * row + 1][2 * right] = value
 
+    def _get_horizontal_boundary_value(self, bottom: int, column: int) -> str:
+        return self._board[2 * bottom][2 * column + 1]
+
     def _set_horizontal_boundary_value(self, bottom: int, column: int,
                                        value: str) -> None:
         self._board[2 * bottom][2 * column + 1] = value
+
+    def _set_boundary_intersection_value(self, bottom: int, right: int,
+                                         value: str) -> None:
+        self._board[2 * bottom][2 * right] = value
 
     def _set_visulization(self):
         self._visualization = '\n'.join([''.join(row) for row in self._board])
