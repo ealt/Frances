1163770d0d120d1341af47b56c6efb9c913288f2
Eric Alt <ericallenalt@gmail.com>
2021-06-11 23:27:59 -0500

    [style] Deduplicate wall values

--- puzzle_visualizer.py
+++ puzzle_visualizer.py
@@ -43,44 +43,46 @@ class PuzzleVisualizer:
         return self._visualization
 
     def _add_walls(self):
+        self._vertical_wall_value = WALL_INTERSECTION_VALUES[WallIntersection(
+            up=True, down=True)]
+        self._horizontal_wall_value = self._w * WALL_INTERSECTION_VALUES[
+            WallIntersection(left=True, right=True)]
+        self._horizontal_empty_value = self._w * WALL_INTERSECTION_VALUES[
+            WallIntersection()]
         self._add_exterior_walls()
         self._add_interior_walls()
         self._add_wall_intersections()
 
     def _add_exterior_walls(self) -> None:
-        vertical_wall_value = '\u2502'
-        horizontal_wall_value = self._w * '\u2500'
-        horizontal_empty_value = self._w * ' '
-        self._board.append(ROW(self._n, horizontal_wall_value))
+        self._board.append(ROW(self._n, self._horizontal_wall_value))
         for r in range(1, 2 * self._n):
-            self._board.append(ROW(self._n, horizontal_empty_value))
+            self._board.append(ROW(self._n, self._horizontal_empty_value))
             if r % 2 == 1:
-                self._board[-1][0] = vertical_wall_value
-                self._board[-1][-1] = vertical_wall_value
-        self._board.append(ROW(self._n, horizontal_wall_value))
+                self._board[-1][0] = self._vertical_wall_value
+                self._board[-1][-1] = self._vertical_wall_value
+        self._board.append(ROW(self._n, self._horizontal_wall_value))
 
     def _add_interior_walls(self) -> None:
         self._add_vertical_interior_walls()
         self._add_horizontal_interior_walls()
 
     def _add_vertical_interior_walls(self) -> None:
-        vertical_wall_value = '\u2502'
         for r, row in enumerate(self._puzzle.crime_scene.floor_plan):
             for c, left, right in zip(range(1, self._n), row.values[:-1],
                                       row.values[1:]):
                 if left != right:
-                    self._set_vertical_boundary_value(r, c, vertical_wall_value)
+                    self._set_vertical_boundary_value(r, c,
+                                                      self._vertical_wall_value)
 
     def _add_horizontal_interior_walls(self) -> None:
-        horizontal_wall_value = self._w * '\u2500'
         for r, top_row, bottom_row in zip(
                 range(1, self._n), self._puzzle.crime_scene.floor_plan[:-1],
                 self._puzzle.crime_scene.floor_plan[1:]):
             for c, top, bottom in zip(range(self._n), top_row.values,
                                       bottom_row.values):
                 if top != bottom:
-                    self._set_horizontal_boundary_value(r, c,
-                                                        horizontal_wall_value)
+                    self._set_horizontal_boundary_value(
+                        r, c, self._horizontal_wall_value)
 
     def _add_wall_intersections(self) -> None:
         for bottom in range(self._n + 1):
@@ -93,16 +95,14 @@ class PuzzleVisualizer:
                                right: int) -> WallIntersection:
         top = bottom - 1
         left = right - 1
-        vertical_wall_value = '\u2502'
-        horizontal_wall_value = self._w * '\u2500'
         up_wall = False if bottom == 0 else self._get_vertical_boundary_value(
-            top, right) == vertical_wall_value
+            top, right) == self._vertical_wall_value
         down_wall = False if bottom == self._n else self._get_vertical_boundary_value(
-            bottom, right) == vertical_wall_value
+            bottom, right) == self._vertical_wall_value
         left_wall = False if right == 0 else self._get_horizontal_boundary_value(
-            bottom, left) == horizontal_wall_value
+            bottom, left) == self._horizontal_wall_value
         right_wall = False if right == self._n else self._get_horizontal_boundary_value(
-            bottom, right) == horizontal_wall_value
+            bottom, right) == self._horizontal_wall_value
         return WallIntersection(up=up_wall,
                                 down=down_wall,
                                 left=left_wall,
