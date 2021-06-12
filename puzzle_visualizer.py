4654f4dacc12ff6aca5acc646c63c8b19aec8465
Eric Alt <ericallenalt@gmail.com>
2021-06-11 22:45:52 -0500

    [feat] Add interior walls to visualization
    resolves #52

--- puzzle_visualizer.py
+++ puzzle_visualizer.py
@@ -11,6 +11,7 @@ class PuzzleVisualizer:
         self._n = len(self._puzzle.people)
         self._board = []
         self._add_exterior_walls()
+        self._add_interior_walls()
         self._set_visulization()
 
     @property
@@ -28,6 +29,37 @@ class PuzzleVisualizer:
                 self._board[-1][0] = vertical_wall_value
                 self._board[-1][-1] = vertical_wall_value
         self._board.append(ROW(self._n, horizontal_wall_value))
+
+    def _add_interior_walls(self) -> None:
+        self._add_vertical_interior_walls()
+        self._add_horizontal_interior_walls()
+
+    def _add_vertical_interior_walls(self) -> None:
+        vertical_wall_value = '\u2502'
+        for r, row in enumerate(self._puzzle.crime_scene.floor_plan):
+            for c, left, right in zip(range(1, self._n), row.values[:-1],
+                                      row.values[1:]):
+                if left != right:
+                    self._set_vertical_boundary_value(r, c, vertical_wall_value)
+
+    def _add_horizontal_interior_walls(self) -> None:
+        horizontal_wall_value = self._w * '\u2500'
+        for r, top_row, bottom_row in zip(
+                range(1, self._n), self._puzzle.crime_scene.floor_plan[:-1],
+                self._puzzle.crime_scene.floor_plan[1:]):
+            for c, top, bottom in zip(range(self._n), top_row.values,
+                                      bottom_row.values):
+                if top != bottom:
+                    self._set_horizontal_boundary_value(r, c,
+                                                        horizontal_wall_value)
+
+    def _set_vertical_boundary_value(self, row: int, right: int,
+                                     value: str) -> None:
+        self._board[2 * row + 1][2 * right] = value
+
+    def _set_horizontal_boundary_value(self, bottom: int, column: int,
+                                       value: str) -> None:
+        self._board[2 * bottom][2 * column + 1] = value
 
     def _set_visulization(self):
         self._visualization = '\n'.join([''.join(row) for row in self._board])
